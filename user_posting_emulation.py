d24feeaed72318f005507210cff868623d61eeb4
Matthew Marquis <matthew_marquis@btinternet.com>
2023-04-19 12:45:11 +0100

    Created function to send data from loops to Kafka via HTTP endpoint exposed by API Gateway

--- user_posting_emulation.py
+++ user_posting_emulation.py
@@ -28,6 +28,13 @@ class AWSDBConnector:
 
 new_connector = AWSDBConnector()
 
+def send_data_to_s3(result_data, topic_ext):
+    invoke_url = f'https://ltt3iaz1xk.execute-api.us-east-1.amazonaws.com/test/topics/0a2bc878981f.{topic_ext}'
+    payload = json.dumps({'records': [{'value': result_data}]}, default=str)
+    headers = {'Content-Type': 'application/vnd.kafka.json.v2+json'}
+    response = requests.request('POST', invoke_url, headers=headers, data=payload)
+    print(response.status_code)
+
 def run_infinite_post_data_loop():
     while True:
         sleep(random.randrange(0, 2))
@@ -42,6 +49,7 @@ def run_infinite_post_data_loop():
             for row in pin_selected_row:
                 pin_data = dict(row._mapping)
                 print(pin_data)
+                send_data_to_s3(pin_data, 'pin')
             
             geo_string = text(f"SELECT * FROM geolocation_data LIMIT {random_row}, 1")
             geo_selected_row = connection.execute(geo_string)
@@ -49,6 +57,7 @@ def run_infinite_post_data_loop():
             for row in geo_selected_row:
                 geo_result = dict(row._mapping)
                 print(geo_result)
+                send_data_to_s3(geo_result, 'geo')
 
             user_string = text(f"SELECT * FROM user_data LIMIT {random_row}, 1")
             user_selected_row = connection.execute(user_string)
@@ -56,6 +65,7 @@ def run_infinite_post_data_loop():
             for row in user_selected_row:
                 user_result = dict(row._mapping)
                 print(user_result)
+                send_data_to_s3(user_result, 'user')
             
 if __name__ == "__main__":
     run_infinite_post_data_loop()
